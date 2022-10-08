95c2c74e62a698a8fb5484d7eecbd03ac70e0726
INNOCENT VIR <102230621+VIR99@users.noreply.github.com>
2022-10-08 21:17:30 +0530

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -21,7 +21,7 @@ loop = asyncio.get_event_loop()
 async def initiate_bot():
     for all_module in ALL_MODULES:
         importlib.import_module("Yukki.modules." + all_module)
-    print("Started Yukki AFK Bot.")
+    print("Started TheAFKRobot.")
     await idle()
     print("GoodBye! Stopping Bot")
 
